bef28d20077a2d58727f505059183f7ef25b9d2f
shaohong <shaohong@shhcode.net>
2013-04-24 18:00:30 +0800

    Updated

--- cio-test.py
+++ cio-test.py
@@ -16,6 +16,10 @@ class PythonOrgSearch(unittest.TestCase):
         self.username = "cio_test"
         self.password = "CIOtest123"
 
+        #Page title
+        self.loginPageTitle = "login page"
+        self.mainPageTiltle = "home page"
+
         #Links for CIO page
         self.url_HomePage = "http://idata-a.d1.comp.nus.edu.sg/home"
 
@@ -23,14 +27,14 @@ class PythonOrgSearch(unittest.TestCase):
     def test_HomePage(self):
         driver = self.driver
         driver.get(self.url_HomePage)
-        self.assertIn("home", driver.title)
+        self.assertIn(self.loginPageTitle, driver.title)
 
 
     #This will be testing logging in with Twitter
     def test_TwitterLogin(self):
         driver = self.driver
         driver.get(self.url_HomePage)
-        self.assertIn("home", driver.title)
+        self.assertIn(self.loginPageTitle, driver.title)
         elem = driver.find_element_by_xpath("//a[@class=\"btn btn-primary btn-large\"]")
         elem.click()
         self.assertIn("Twitter / Authorize an application", driver.title)
@@ -40,23 +44,24 @@ class PythonOrgSearch(unittest.TestCase):
         elem_password.send_keys(self.password)
         driver.find_element_by_id("allow").click()
         time.sleep(2)
-        self.assertIn("home", driver.title)
+        self.assertIn(self.mainPageTiltle, driver.title)
 
     def test_Logout(self):
         self.test_TwitterLogin()
         driver = self.driver
         driver.get(self.url_HomePage)
-        driver.find_element_by_id("dLabel").click()
+        find_element_by_id("dLabel").click()
         time.sleep(0.5)
         elem = driver.find_element_by_xpath("//a[@id=\"logout\"]").click()
         time.sleep(2)
-        self.assertIn("home", driver.title)
+        self.assertIn(self.loginPageTitle, driver.title)
 
     #This will testing the main page after logging in
     def test_MainPage(self):
         self.test_TwitterLogin()
         driver = self.driver
         driver.get(self.url_HomePage)
+        time.sleep(3)
         try:
             driver.find_element_by_xpath("//a[contains(@href,'#tab1')]")
         except NoSuchElementException:
@@ -75,6 +80,29 @@ class PythonOrgSearch(unittest.TestCase):
             driver.find_element_by_xpath("//a[contains(@href,'/home/en/?title=Singapore')]")
         except NoSuchElementException:
             assert 0, "Did not return correct search result"
+
+    def test_doubleSearch(self):
+        self.test_Search()
+        driver = self.driver
+        driver.get(self.url_HomePage)
+        elem = driver.find_element_by_xpath("//input[@class=\"search-query span2\"]")
+        elem.send_keys("NUS")
+        elem.send_keys(Keys.RETURN)
+        time.sleep(2)
+        try:
+            driver.find_element_by_xpath("//a[contains(@href,'/home/en/?title=NUS')]")
+        except NoSuchElementException:
+            assert 0, "Did not return correct search result"
+
+    def test_MoreTweets(self):
+        self.test_TwitterLogin()
+        driver = self.driver
+        driver.get(self.url_HomePage)
+        elem = driver.find_element_by_link_text("Wikipedia")
+        elem.click()
+        time.sleep(10)
+        elem = driver.find_element_by_id("loadMore")
+        self.assertIn("No New Tweets", driver.find_element_by_id("loadMore").text)
 
 
     #This will end the test suite
